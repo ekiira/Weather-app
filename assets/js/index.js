c78ea0391881d66740563dd7966b55480539c748
ekiira <jayoluwatosin@gmail.com>
2020-09-07 03:21:37 +0100

    retrieved data from local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,12 +13,8 @@ const weatherUvIndex = document.querySelector('.uvi')
 const weatherClouds = document.querySelector('.clouds')
 const date = document.querySelector('.date')
 const weatherCard = document.querySelectorAll('.det')
-
-
 const background = document.querySelector('.bbg')
-
 const loader = document.querySelector(".loader");
-
 
 let lat;
 let lon;
@@ -26,28 +22,33 @@ let lon;
 form.addEventListener('submit', async (e) => {  
   e.preventDefault();
  
+  const searchData = namee.value;
+  if(searchData) {
+    loader.style.display = 'block';
+  loader.classList.remove('hidden')
+  }
 
-const searchData = namee.value;
-if(searchData) {
-  loader.style.display = 'block';
-loader.classList.remove('hidden')
-}
-
-
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-const url1 = `http://api.openweathermap.org/data/2.5/weather?q=${searchData}&units=metric&APPID=${API_KEY}`
-const response = await fetch(proxyUrl + url1 , {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin' : '*',
-    }
-  });
+  // Fetch longitude and latitude 
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const url1 = `http://api.openweathermap.org/data/2.5/weather?q=${searchData}&units=metric&APPID=${API_KEY}`
+  const response = await fetch(proxyUrl + url1 , {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin' : '*',
+      }
+    });
 
   const finalResponse = await response.json();
   lat = finalResponse.coord.lat;
-  lon = finalResponse.coord.lon
+  lon = finalResponse.coord.lon;
+
+  // Save data to localstorage
+  localStorage.setItem('currentCity', finalResponse.name); 
+
+  // set search input to initial state
   namee.value = ''
-  weatherCity.innerHTML = finalResponse.name;
+
+  // Fetch current weather info
   if(lat || lon) {
     const url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${API_KEY}`
 
@@ -59,96 +60,109 @@ const response = await fetch(proxyUrl + url1 , {
       });
       const finalResponse2 = await response2.json();
 
-      if (finalResponse2) {
-        loader.classList.add('hidden');
-        loader.style.display = 'none';
-      }
       
+      // Save data to localstorage
       localStorage.setItem('allCurrentData', JSON.stringify(finalResponse2.current)); 
-     
-      const currentData = JSON.parse(localStorage.getItem('allCurrentData') )
+    } 
+  });
+  
+  // Retrieve data from localstorage
+  weatherCity.innerHTML = localStorage.getItem('currentCity')
+  const currentData = JSON.parse(localStorage.getItem('allCurrentData') )
 
-      if(currentData) {
-        let i;
-        for (i = 0; i < weatherCard.length; i++) {
-          weatherCard[i].style.display = 'block'
-        };
+  // if (finalResponse2) {
+  //   loader.classList.add('hidden');
+  //   loader.style.display = 'none';
+  // }
+  if (localStorage.length > 0){
+    console.log('yes')
+    //Items are stored in local stor age
+}else{
+  console.log('no')
 
-        weatherIcon.style.display = 'block'
-      }
+    //Local storage is empty
+}
+console.log('dd', currentData)
 
-      const wDegree = Math.round(Number(currentData.temp))
-      const wIcon = currentData.weather[0].icon
-      const wState = currentData.weather[0].description
-      const wStateMain = currentData.weather[0].main
-      const humidity =  Math.round(Number(currentData.humidity))
-      const feelsLike = Math.round(Number(currentData.feels_like))
-      const uv = Math.round(Number(currentData.uvi))
-      const clouds = Math.round(Number(currentData.clouds))
-      const timestamp = currentData.dt
-      const currentDate = new Date(timestamp * 1000).toUTCString()
-      
-      switch (true) {
-        case (wStateMain.includes('Clouds')):
-          background.style.backgroundImage = "url('../../assets/images/clouds.jpeg')"
-          break;
-        case (wStateMain.includes('Rain')):
-          background.style.backgroundImage = "url('../../assets/images/rain.jpeg')"
-          break;
-        case (wStateMain.includes('Thunderstorm')):
-          background.style.backgroundImage = "url('../../assets/images/storm.jpeg')"
-          break;
-        case (wStateMain.includes('Drizzle')):
-          background.style.backgroundImage = "url('../../assets/images/drizzle.jpeg')"
-          break;
-        case (wStateMain.includes('Snow')):
-          background.style.backgroundImage = "url('../../assets/images/snow.jpeg')"
-          break;
-        case (wStateMain.includes('Clear')):
-          background.style.backgroundImage = "url('../../assets/images/clear-sky.jpeg')"
-          break;
-        default:
-          background.style.backgroundImage = "url('../../assets/images/bg-2.jpeg')"
-          break;
-      }
+  if(currentData) {
+    loader.classList.add('hidden');
+    loader.style.display = 'none';
+    let i;
+    for (i = 0; i < weatherCard.length; i++) {
+      weatherCard[i].style.display = 'block'
+    }
+  weatherIcon.style.display = 'block'
 
-     
+  const wDegree = Math.round(Number(currentData.temp))
+  const wIcon = currentData.weather[0].icon
+  const wState = currentData.weather[0].description
+  const wStateMain = currentData.weather[0].main
+  const humidity =  Math.round(Number(currentData.humidity))
+  const feelsLike = Math.round(Number(currentData.feels_like))
+  const uv = Math.round(Number(currentData.uvi))
+  const clouds = Math.round(Number(currentData.clouds))
+  const timestamp = currentData.dt
+  const currentDate = new Date(timestamp * 1000).toUTCString()
 
-      let uvState;
-      switch (true) {
-          case (uv <= 2):
-              uvState = 'Low'
-              break;
-          case (uv <= 5):
-              uvState = 'Moderate'
-              break;
-          case (uv <= 7):
-              uvState = 'High'
-              break;
-          case (uv <= 10):
-            uvState = 'Very High'
-            break;
-          case (uv >= 11):
-            uvState = 'Extreme'
-            break;
-          default:
-              uvState = ''
-              break;
-      }
-    
-      
-      weatherDegree.innerHTML = `${wDegree}&#176`
-      weatherIcon.src = `http://openweathermap.org/img/wn/${wIcon}@2x.png`
-      weatherState.innerHTML = wState
-      weatherHumidity.innerHTML =  0 ? humidity : `${humidity}%`
-     
-      weatherFeelsLike.innerHTML = `${feelsLike}&#176;`
-      weatherUvIndex.innerHTML = uvState
-      weatherClouds.innerHTML = 0 ? clouds : `${clouds}%`
-      date.innerHTML = currentDate.split(' ').slice(0, 4).join(' ');
-      console.log('dddd', finalResponse2, wIcon)
-      
+  // Background change based on weather info
+  switch (true) {
+    case (wStateMain.includes('Clouds')):
+      background.style.backgroundImage = "url('../../assets/images/clouds.jpeg')"
+      break;
+    case (wStateMain.includes('Rain')):
+      background.style.backgroundImage = "url('../../assets/images/rain.jpeg')"
+      break;
+    case (wStateMain.includes('Thunderstorm')):
+      background.style.backgroundImage = "url('../../assets/images/storm.jpeg')"
+      break;
+    case (wStateMain.includes('Drizzle')):
+      background.style.backgroundImage = "url('../../assets/images/drizzle.jpeg')"
+      break;
+    case (wStateMain.includes('Snow')):
+      background.style.backgroundImage = "url('../../assets/images/snow.jpeg')"
+      break;
+    case (wStateMain.includes('Clear')):
+      background.style.backgroundImage = "url('../../assets/images/clear-sky.jpeg')"
+      break;
+    default:
+      background.style.backgroundImage = "url('../../assets/images/bg-2.jpeg')"
+      break;
   }
 
-});
-  
+
+  // UV Index evaluation
+  let uvState;
+  switch (true) {
+      case (uv <= 2):
+          uvState = 'Low'
+          break;
+      case (uv <= 5):
+          uvState = 'Moderate'
+          break;
+      case (uv <= 7):
+          uvState = 'High'
+          break;
+      case (uv <= 10):
+        uvState = 'Very High'
+        break;
+      case (uv >= 11):
+        uvState = 'Extreme'
+        break;
+      default:
+          uvState = ''
+          break;
+}
+
+
+weatherDegree.innerHTML = `${wDegree}&#176`
+weatherIcon.src = `http://openweathermap.org/img/wn/${wIcon}@2x.png`
+weatherState.innerHTML = wState
+weatherHumidity.innerHTML =  0 ? humidity : `${humidity}%`
+
+weatherFeelsLike.innerHTML = `${feelsLike}&#176;`
+weatherUvIndex.innerHTML = uvState
+weatherClouds.innerHTML = 0 ? clouds : `${clouds}%`
+date.innerHTML = currentDate.split(' ').slice(0, 4).join(' ');
+}
+
+
