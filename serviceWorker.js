84d1f9a55fc3f1c6fc2e29d6ebf5803114beecb4
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-09-09 15:42:11 +0100

    cached assets

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,50 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/assets/js/app.js',
+  '/assets/js/index.js',
+  '/assets/css/style.css',
+  '/assets/images/bg-2.jpeg',
+  '/assets/images/clear-sky.jpeg',
+  '/assets/images/clouds.jpeg',
+  '/assets/images/drizzle.jpeg',
+  '/assets/images/rain.jpeg',
+  '/assets/images/snow.jpeg',
+  '/assets/images/storm.jpeg',
+  '/assets/icons/favicon-16x16.png',
+  '/assets/icons/favicon-32x32.png',
+  '/assets/icons/favicon.ico'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed')
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 })
 
 // activate service worker
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated')
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 })
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt)
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 })
